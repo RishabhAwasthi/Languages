44ba4f3e7f7e506eb4d136bf70b6a6d958312a5b
rishabh <rishabh7awashi@gmail.com>
2017-09-29 18:38:31 +0530

    swipe added

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,11 +15,9 @@
  */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // Set the content of the activity to use the activity_main.xml layout file
-        setContentView(R.layout.activity_main);
+        /*setContentView(R.layout.activity_main);
 
         TextView numbers =(TextView) findViewById(R.id.numbers);
         numbers.setOnClickListener(new View.OnClickListener() {
@@ -65,4 +63,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+}*/
+        setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+    }
 }
+
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,125 +1,17 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
-import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
-import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK;
+import android.support.v7.app.AppCompatActivity;
 
 public class PhrasesActivity extends AppCompatActivity {
 
-    private MediaPlayer mMediaPlayer;
-    private AudioManager audioManager;
 
-    AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        public void onAudioFocusChange(int focusChange) {
-            if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT || focusChange==AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
-                mMediaPlayer.pause();
-                mMediaPlayer.seekTo(0);
-                // Pause playback
-            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                mMediaPlayer.start();
-            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                releaseMediaPlayer();
-            }
-        }
-    };
-    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener(){
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            releaseMediaPlayer();
-        }
-    };
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
-        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-
-
-
-        // Create a list of words
-                final ArrayList<Word> words = new ArrayList<Word>();
-                words.add(new Word("Hello", "O-la",R.raw.hello));
-                words.add(new Word("Good morning", "Bway-nos Dee-as",R.raw.goodmorning));
-                words.add(new Word("What is your name", "Commo te-ya-mas",R.raw.namewhat));
-                words.add(new Word("My name is..", "Me nombre-es..",R.raw.myname));
-                words.add(new Word("How are you feeling", "commo te-si-entes?",R.raw.howu));
-                words.add(new Word("I'm feeling good", "mey-si-ento-bien",R.raw.good));
-                words.add(new Word("Are you coming?", "Vienes?",R.raw.comingaa));
-                words.add(new Word("Yes,I'm coming", "Si,voy para-aiya",R.raw.yescoming));
-                words.add(new Word("I'm coming", "Ya voi",R.raw.coming));
-                words.add(new Word("Lets go", "Vamonos",R.raw.letsgo));
-                words.add(new Word("Come here", "ven-aka",R.raw.come));
-
-
-                // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-                // adapter knows how to create list items for each item in the list.
-
-        WordAdapter itemsAdapter = new WordAdapter(this, words);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(itemsAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // Get the {@link Word} object at the given position the user clicked on
-                Word word = words.get(position);
-                releaseMediaPlayer();
-                // Request audio focus for playback
-                int result = audioManager.requestAudioFocus(afChangeListener,
-                        // Use the music stream.
-                        AudioManager.STREAM_MUSIC,
-                        // Request permanent focus.
-                        AudioManager.AUDIOFOCUS_GAIN);
-
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-
-                    // Start playback.
-
-
-                    // Create and setup the {@link MediaPlayer} for the audio resource associated
-                    // with the current word
-                    mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getAudioResourceId());
-
-                    // Start the audio file
-                    mMediaPlayer.start();
-                    mMediaPlayer.setOnCompletionListener(completionListener );
-                }
-            }
-        });
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PhrasesFragment())
+                .commit();
     }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mMediaPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-            audioManager.abandonAudioFocus(afChangeListener);
-        }
-    }
-
-
-
 }
-
